e91c6bb1b581d311ee4a1539ebf9236f84ffe17d
fivethreeo <oyvind.saltvik@gmail.com>
2011-09-13 10:46:04 +0200

    missing import

--- filer_gallery/admin.py
+++ filer_gallery/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from django.forms.fields import Field
+from django import forms
+
 from django.utils import simplejson
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
