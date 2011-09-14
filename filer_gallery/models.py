f0572771c85979cad8e8f99d27d44eb0bc23fdab
fivethreeo <oyvind.saltvik@gmail.com>
2011-09-14 15:42:17 +0200

    add pub date

--- filer_gallery/models.py
+++ filer_gallery/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 from django.db import models
 from filer.fields.image import FilerImageField
 
@@ -6,7 +7,8 @@ class Gallery(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     category = models.ForeignKey('categories.Category')
-
+    pub_date = models.DateTimeField(default=datetime.now)
+    
     def __unicode__(self):
         return self.title
     
@@ -14,6 +16,7 @@ class GalleryImage(models.Model):
     gallery = models.ForeignKey(Gallery)
     title = models.CharField(max_length=255)
     category = models.ForeignKey('categories.Category', null=True, blank=True)
+    pub_date = models.DateTimeField(default=datetime.now)
     image = FilerImageField()
 
     def __unicode__(self):
