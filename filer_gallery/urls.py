2133043e06b99d574f79d9bd2fdc33cd55600eaf
fivethreeo <oyvind.saltvik@gmail.com>
2011-09-14 15:26:10 +0200

    views and urls

--- filer_gallery/urls.py
+++ filer_gallery/urls.py
@@ -1,6 +1,83 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
+from filer_gallery.views import CategoryAllRelatedList, ImageViaGalleryCategoryList
+from django.views.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView
 
-urlpatterns = patterns('',
-    (r'^$', '%(module).views.default'),
+image_info_dict = {
+    'queryset': GalleryImage.objects.all(),
+    'date_field': 'pub_date'
+}
+
+image_info_month_dict = {
+    'queryset': GalleryImage.objects.all(),
+    'date_field': 'pub_date',
+    'month_format': '%m'
+}
+
+image_info_year_dict = {
+    'queryset': GalleryImage.objects.all(),
+    'date_field': 'pub_date'
+}
+gallery_info_dict = {
+    'queryset': Gallery.objects.all(),
+    'date_field': 'pub_date'
+}
+
+gallery_info_month_dict = {
+    'queryset': Gallery.objects.all(),
+    'date_field': 'pub_date',
+    'month_format': '%m'
+}
+
+gallery_info_year_dict = {
+    'queryset': Gallery.objects.all(),
+    'date_field': 'pub_date'
+}
+
+
+urlpatterns += patterns('',
+
+    url(r'^$',
+        ArchiveIndexView.as_view(**image_info_dict),
+        name= 'filer_gallery_galleryimage_archive_index'),
+        
+    url(r'^images/(?P<year>\d{4})/$', 
+        YearArchiveView.as_view(**image_info_year_dict),
+        name= 'filer_gallery_galleryimage_archive_year'),
+    
+    url(r'^images/(?P<year>\d{4})/(?P<month>\d{2})/$', 
+        MonthArchiveView.as_view(**image_info_month_dict),
+        name= 'filer_gallery_galleryimage_archive_month'),
+    
+    url(r'^images/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 
+        DayArchiveView.as_view(**image_info_month_dict),
+        name= 'filer_gallery_galleryimage_archive_day'),
+        
+    url(r'^images/(?P<category_path>.+)/$',
+        ImageViaGalleryCategoryList.as_view(model=GalleryImage),
+        name='filer_gallery_galleryimage_gallery_category'),
+        
+    url(r'^images/(?P<category_path>.+)/$',
+        CategoryAllRelatedList.as_view(model=GalleryImage),
+        name='filer_gallery_galleryimage_category'),
+        
+    url(r'^galleries/$',
+        ArchiveIndexView.as_view(**gallery_info_dict),
+        name= 'filer_gallery_gallery_archive_index'),
+    
+    url(r'^galleries/(?P<year>\d{4})/$', 
+        YearArchiveView.as_view(**gallery_info_year_dict),
+        name= 'filer_gallery_gallery_archive_year'),
+    
+    url(r'^galleries/(?P<year>\d{4})/(?P<month>\d{2})/$', 
+        MonthArchiveView.as_view(**gallery_info_month_dict),
+        name= 'filer_gallery_gallery_archive_month'),
+    
+    url(r'^galleries/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 
+        DayArchiveView.as_view(**gallery_info_month_dict),
+        name= 'filer_gallery_gallery_archive_day'),
+        
+    url(r'^galleries/(?P<category_path>.+)/$',
+        CategoryAllRelatedList.as_view(model=Gallery),
+        name='filer_gallery_galleryimage_category')              
 )
--- filer_gallery/views.py
+++ filer_gallery/views.py
@@ -1,1 +1,48 @@
 # -*- coding: utf-8 -*-
+try:
+    from django.views.generic import ListView
+except ImportError:
+    from cbv import ListView
+    
+from categories.views import get_category_for_path
+
+class CategoryAllRelatedList(ListView):
+    
+    category_field = 'category'
+    
+    def get_queryset(self):
+        queryset = super(CategoryAllRelatedList, self).get_queryset()
+        category = get_category_for_path(self.kwargs['category_path'])
+        kwargs = {
+            '%s__tree_id' % self.category_field:
+            '%s__lft__gte' % self.category_field: category.lft,
+            '%s__rgt__lte' % self.category_field: category.rgt
+        }
+        return queryset.filter(**kwargs).order_by('%s__lft' % self.category_field)
+
+    def get_template_names(self):
+        names = []
+        if hasattr(self.object_list, 'model'):
+            opts = self.object_list.model._meta
+            path_items = self.kwargs[self.path_field].strip('/').split('/')
+            while path_items:
+                names.append( '%s/category_all_%s_%s%s.html' % (opts.app_label,
+                                                            '_'.join(path_items),
+                                                            opts.object_name.lower(),
+                                                            self.template_name_suffix)
+                                                            )
+                path_items.pop()
+            names.append('%s/category_all_%s%s.html' % (opts.app_label,
+                                                    opts.object_name.lower(),
+                                                    self.template_name_suffix)
+                                                    )
+        names.extend(super(CategoryAllRelatedList, self).get_template_names())
+        return names
+        
+    def get_context_data(self, **kwargs):
+        context = super(CategoryAllRelatedList, self).get_context_data(**kwargs)
+        context['category'] = get_category_for_path(self.kwargs['category_path'])
+        return context
+
+class ImageViaGalleryCategoryList(CategoryAllRelatedList):
+    category_field = 'gallery__category'
