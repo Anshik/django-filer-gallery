c6d207147581adccfa4ce3ce25185c09d8fc3e32
fivethreeo <oyvind.saltvik@gmail.com>
2011-09-14 18:14:09 +0200

    config

--- filer_gallery/views.py
+++ filer_gallery/views.py
@@ -59,13 +59,29 @@ class ImageViaGalleryCategoryList(CategoryAllRelatedList):
     category_field = 'gallery__category'
 
 class GalleryArchiveIndexView(ArchiveIndexView, ConfigMixin):
-    pass
+    def get_context_data(self, **kwargs):
+        context = super(GalleryArchiveIndexView, self).get_context_data(**kwargs)
+        context['ORBIT_CONFIG'] = simplejson.dumps(filer_gallery_settings.ORBIT_CONFIG)
+        context['FILER_GALLERY_DISPLAY_SIZE'] = filer_gallery_settings.FILER_GALLERY_DISPLAY_SIZE
+        return context
     
 class GalleryYearArchiveView(YearArchiveView, ConfigMixin):
-    pass
+    def get_context_data(self, **kwargs):
+        context = super(GalleryYearArchiveView, self).get_context_data(**kwargs)
+        context['ORBIT_CONFIG'] = simplejson.dumps(filer_gallery_settings.ORBIT_CONFIG)
+        context['FILER_GALLERY_DISPLAY_SIZE'] = filer_gallery_settings.FILER_GALLERY_DISPLAY_SIZE
+        return context
     
 class GalleryMonthArchiveView(MonthArchiveView, ConfigMixin):
-    pass
+    def get_context_data(self, **kwargs):
+        context = super(GalleryMonthArchiveView, self).get_context_data(**kwargs)
+        context['ORBIT_CONFIG'] = simplejson.dumps(filer_gallery_settings.ORBIT_CONFIG)
+        context['FILER_GALLERY_DISPLAY_SIZE'] = filer_gallery_settings.FILER_GALLERY_DISPLAY_SIZE
+        return context
     
 class GalleryDayArchiveView(DayArchiveView, ConfigMixin):
-    pass
+    def get_context_data(self, **kwargs):
+        context = super(GalleryMonthArchiveView, self).get_context_data(**kwargs)
+        context['ORBIT_CONFIG'] = simplejson.dumps(filer_gallery_settings.ORBIT_CONFIG)
+        context['FILER_GALLERY_DISPLAY_SIZE'] = filer_gallery_settings.FILER_GALLERY_DISPLAY_SIZE
+        return context
