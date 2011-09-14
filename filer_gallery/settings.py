2c67c7e538fe1430f08f54bbf8b7b1825af42d68
fivethreeo <oyvind.saltvik@gmail.com>
2011-09-14 18:33:57 +0200

    config

--- filer_gallery/settings.py
+++ filer_gallery/settings.py
@@ -1,4 +1,18 @@
 from django.conf import settings
 
-ORBIT_CONFIG = getattr(settings, 'ORBIT_CONFIG', {})
+ORBIT_CONFIG = getattr(settings, 'ORBIT_CONFIG', {
+    'advanceSpeed': 4000,
+    'animation': 'horizontal-slide',
+    'animationSpeed': 600,
+    'bullets': False,
+    'captionAnimation': 'none',
+    'captionAnimationSpeed': 800,
+    'captions': False,
+    'directionalNav': False,
+    'pauseOnHover': True,
+    'startClockOnMouseOut': True,
+    'startClockOnMouseOutAfter': 1000,
+    'timer': True
+})
+
 FILER_GALLERY_DISPLAY_SIZE = getattr(settings, 'FILER_GALLERY_DISPLAY_SIZE', (500, 300) )
