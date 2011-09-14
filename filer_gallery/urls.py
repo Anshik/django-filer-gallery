ff8d6dc472a826e35b1630acdc736b47e84d3ee3
fivethreeo <oyvind.saltvik@gmail.com>
2011-09-14 18:05:24 +0200

    Gallery

--- filer_gallery/urls.py
+++ filer_gallery/urls.py
@@ -41,19 +41,19 @@ gallery_info_year_dict = {
 urlpatterns = patterns('',
 
     url(r'^$',
-        ArchiveIndexView.as_view(**image_info_dict),
+        GalleryArchiveIndexView.as_view(**image_info_dict),
         name= 'filer_gallery_galleryimage_archive_index'),
         
     url(r'^images/(?P<year>\d{4})/$', 
-        YearArchiveView.as_view(**image_info_year_dict),
+        GalleryYearArchiveView.as_view(**image_info_year_dict),
         name= 'filer_gallery_galleryimage_archive_year'),
     
     url(r'^images/(?P<year>\d{4})/(?P<month>\d{2})/$', 
-        MonthArchiveView.as_view(**image_info_month_dict),
+        GalleryMonthArchiveView.as_view(**image_info_month_dict),
         name= 'filer_gallery_galleryimage_archive_month'),
     
     url(r'^images/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 
-        DayArchiveView.as_view(**image_info_month_dict),
+        GalleryDayArchiveView.as_view(**image_info_month_dict),
         name= 'filer_gallery_galleryimage_archive_day'),
         
     url(r'^images/(?P<category_path>.+)/$',
@@ -65,19 +65,19 @@ urlpatterns = patterns('',
         name='filer_gallery_galleryimage_category'),
         
     url(r'^galleries/$',
-        ArchiveIndexView.as_view(**gallery_info_dict),
+        GalleryArchiveIndexView.as_view(**gallery_info_dict),
         name= 'filer_gallery_gallery_archive_index'),
     
     url(r'^galleries/(?P<year>\d{4})/$', 
-        YearArchiveView.as_view(**gallery_info_year_dict),
+        GalleryYearArchiveView.as_view(**gallery_info_year_dict),
         name= 'filer_gallery_gallery_archive_year'),
     
     url(r'^galleries/(?P<year>\d{4})/(?P<month>\d{2})/$', 
-        MonthArchiveView.as_view(**gallery_info_month_dict),
+        GalleryMonthArchiveView.as_view(**gallery_info_month_dict),
         name= 'filer_gallery_gallery_archive_month'),
     
     url(r'^galleries/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 
-        DayArchiveView.as_view(**gallery_info_month_dict),
+        GalleryDayArchiveView.as_view(**gallery_info_month_dict),
         name= 'filer_gallery_gallery_archive_day'),
         
     url(r'^galleries/(?P<category_path>.+)/$',
