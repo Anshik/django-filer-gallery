32a47914a822312ad7aad3b7601dc78d41232360
fivethreeo <oyvind.saltvik@gmail.com>
2011-09-16 11:52:03 +0200

    pretty

--- filer_gallery/templatetags/filer_gallery_tags.py
+++ filer_gallery/templatetags/filer_gallery_tags.py
@@ -19,13 +19,13 @@ def render_galleryimage_month_links(context):
 @register.inclusion_tag('filer_gallery/gallery_categories_links_snippet.html', takes_context=True)
 def render_gallery_month_links(context):
     return {
-        'category': context.get('category', None)
+        'category': context.get('category', None),
         'categories': Category.objects.all()
     }
 
 @register.inclusion_tag('filer_gallery/galleryimage_categories_links_snippet.html', takes_context=True)
 def render_galleryimage_month_links(context):
     return {
-        'category': context.get('category', None)
+        'category': context.get('category', None),
         'categories': Category.objects.all()
     }
