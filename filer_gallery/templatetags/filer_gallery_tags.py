cf9d635f3132e9681cad1e20076ad332880b28c6
fivethreeo <oyvind.saltvik@gmail.com>
2011-09-16 11:25:10 +0200

    pretty

--- filer_gallery/templatetags/filer_gallery_tags.py
+++ filer_gallery/templatetags/filer_gallery_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from filer_gallery.models import Gallery, GalleryImage
+from categories.models import Category
 
 register = template.Library()
 
@@ -13,4 +14,18 @@ def render_gallery_month_links(context):
 def render_galleryimage_month_links(context):
     return {
         'dates': GalleryImage.objects.dates('pub_date', 'month')
+    }
+    
+@register.inclusion_tag('filer_gallery/gallery_categories_links_snippet.html', takes_context=True)
+def render_gallery_month_links(context):
+    return {
+        'category': context.get('category', None)
+        'categories': Category.objects.all()
+    }
+
+@register.inclusion_tag('filer_gallery/galleryimage_categories_links_snippet.html', takes_context=True)
+def render_galleryimage_month_links(context):
+    return {
+        'category': context.get('category', None)
+        'categories': Category.objects.all()
     }
